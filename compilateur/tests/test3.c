60405b9d7b0014e7c5f6869ce846aabe54e51e67
Clem9 <clementine.bonneau99@orange.fr>
2021-05-25 13:33:12 +0200

    MAJ tests

--- compilateur/tests/test3.c
+++ compilateur/tests/test3.c
@@ -1,4 +1,17 @@
-int main() {
-	int a;
-	a = 1+2;
+int main()
+{ int i, j,k ,r;
+i =3 ;
+j=4 ;
+k=8 ;
+r=(i+j)*(i+k/j) ; 
+if (k==6){ j=2;}
+else
+{
+	j=1;
+}
+printf ( r ) ;
+while((j==1) || (k<4)){
+	k=k+1;
+}
+printf ( i ) ;
 }
--- compilateur/tests/test6.c
+++
@@ -1,6 +0,0 @@
-int main() {
-	int c;
-	int a = 3;
-	int b;
-	b = 9 + a*2;
-}
